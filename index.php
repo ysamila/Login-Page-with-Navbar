068c73d5d2b087c372f4c1b9b3b67822c710bbde
ysamila <alyssasantosmila@gmail.com>
2023-03-28 12:03:39 +0800

    Added a comment from my local device

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <!-- Hello I'm a commit from github or from my peers  -->
+<!-- This is a comment from my local device -->
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
